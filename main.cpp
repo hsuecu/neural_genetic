607bd8619bb2d0420a745fd1f8e547bcb7d9ab4c
hsuecu <himanshu2832001sharma@gmail.com>
2022-10-24 17:36:14 +0530

    made base classes

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main(int argc, const char ** argv) {
+
+
+    
+
+    return 0;
+}
---
+++ model.cpp
@@ -0,0 +1,9 @@
+class Model{
+    class Node {
+        public:
+        int layerId = 0;
+        float inputSum = 0;
+        float output = 0;
+        int nodeId = 0;
+    };
+};
